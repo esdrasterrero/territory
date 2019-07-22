c6802866876638a0f07a92a5a307ba6b263cc956
Esdras Terrero <e.terrero@youwe.nl>
2019-07-23 01:08:03 +0200

    [FEATURE] Simplify easyprint library by just attaching the required library. Simplify js file.

--- web/modules/custom/leaflet_easyprint/src/LeafletEasyprintService.php
+++ web/modules/custom/leaflet_easyprint/src/LeafletEasyprintService.php
@@ -23,39 +23,18 @@ class LeafletEasyprintService extends LeafletService {
    *   Returns a config array for rendering.
    */
   public function leafletRenderMap(array $map, array $features = [], $height = '400px') {
-    // Added parent function, because it loads libraries through php,
-    // inhibiting 'libraries-override'.
-    $map_id = isset($map['id']) ? $map['id'] : Html::getUniqueId('leaflet_map');
-    $attached_libraries = ['leaflet_easyprint/leaflet-drupal', 'leaflet/general'];
     // Attach the EasyPrint library.
-    $attached_libraries[] = 'leaflet_easyprint/leaflet.easyprint';
-    // Add the Leaflet Fullscreen library, if requested.
-    if (isset($map['settings']['fullscreen_control'])) {
-      $attached_libraries[] = 'leaflet/leaflet.fullscreen';
-    }
-    // Add the Leaflet Markecluster library and functionalities, if requested.
-    if ($this->moduleHandler->moduleExists('leaflet_markercluster') && isset($map['settings']['leaflet_markercluster']) && $map['settings']['leaflet_markercluster']['control']) {
-      $attached_libraries[] = 'leaflet_markercluster/leaflet-markercluster';
-      $attached_libraries[] = 'leaflet_markercluster/leaflet-markercluster-drupal';
-    }
-    $settings[$map_id] = [
-      'mapid' => $map_id,
-      'map' => $map,
-      // JS only works with arrays, make sure we have one with numeric keys.
-      'features' => array_values($features),
-    ];
-    return [
-      '#theme' => 'leaflet_map',
-      '#map_id' => $map_id,
-      '#height' => $height,
-      '#map' => $map,
-      '#attached' => [
-        'library' => $attached_libraries,
-        'drupalSettings' => [
-          'leaflet' => $settings,
-        ],
-      ],
-    ];
+    $build = parent::leafletRenderMap($map, $features, $height);
+    $build["#attached"]["library"][] = 'leaflet_easyprint/leaflet.easyprint';
+    $build["#attached"]["library"][] = 'leaflet_easyprint/leaflet-drupal';
+    return $build;
+  }
+
+  /**
+   * Magic method to return any method call inside the inner service.
+   */
+  public function __call($method, $args) {
+    return call_user_func_array(array($this->leafletService, $method), $args);
   }
 
 }
