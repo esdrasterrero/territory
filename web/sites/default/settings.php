b92f8fd917ee4916a4414e938b7fc28859021ad8
Esdras Terrero <esdrasterrero89@gmail.com>
2022-08-29 23:25:29 +0200

    FEATURE | Install ddev

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -782,3 +782,8 @@ $config_directories['sync'] = '../config/sync';
 if (file_exists($app_root . '/' . $site_path . '/settings.env.php')) {
   include $app_root . '/' . $site_path . '/settings.env.php';
 }
+// Automatically generated include for settings managed by ddev.
+$ddev_settings = dirname(__FILE__) . '/settings.ddev.php';
+if (getenv('IS_DDEV_PROJECT') == 'true' && is_readable($ddev_settings)) {
+  require $ddev_settings;
+}
